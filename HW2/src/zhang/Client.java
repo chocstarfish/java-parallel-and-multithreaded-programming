fe22dec84fd7f36845be45ddc978d3347a11b789
Nicolas ZHANG <nicolasloco@gmail.com>
2015-09-23 00:54:16 -0400

    initial commit for HW2

---
+++ HW2/src/zhang/Client.java
@@ -0,0 +1,6 @@
+package zhang;
+
+public interface Client extends Runnable
+{
+	String name();
+}
---
+++ HW2/src/zhang/Main.java
@@ -0,0 +1,29 @@
+package zhang;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AuctionServer auctionServer = AuctionServer.getInstance();
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+        auctionServer.submitItem("shuai","A nice car",20000,30000);
+    }
+}
